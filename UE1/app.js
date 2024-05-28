d034b7122fd509faa9687952fe18f4f197bc5053
Reitmania <reiti@reitmania.de>
2024-05-28 13:28:49 +0200

    Add files via upload

---
+++ UE1/app.js
@@ -0,0 +1,21 @@
+import * as THREE from '../three.module.js';
+import { OrbitControls } from '../OrbitControls.js';
+
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+    
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
+    
+    resize(){
+        
+    }
+    
+	render( ) {   
+        
+    }
+}
+
+export { App };
